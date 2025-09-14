178c45fd9174385e9fc62802593a752a0de9fc20
AdityaBhairawkar <adityabhairawkar@gmail.com>
2025-09-14 17:12:11 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -80,8 +80,8 @@ def delete_todo(todo_id):
     conn.close()
     return jsonify({'success': True})
 
-init_db()
 
 if __name__ == '__main__':
+    init_db()
     # Listen on all interfaces so frontend EC2 can reach it
     app.run(host='0.0.0.0', port=5000)
